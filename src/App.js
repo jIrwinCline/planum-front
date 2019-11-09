04f9d012682c96a8fdb3e2d50bda29648fcc1c89
jirwincline <jakobinwins@gmail.com>
2019-11-09 14:35:59 -0800

    added pages home and login. installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+//pages
+import home from './pages/home';
+import login from './pages/login';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Our App</h1>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={home}/>
+        <Route path='/login' component={login}/>
+      </Switch>
+    </Router>
     </div>
   );
 }
