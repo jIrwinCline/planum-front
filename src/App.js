93a650a885b02d32adb84c11061176cbae0db223
jirwincline <jakobinwins@gmail.com>
2019-11-10 14:52:10 -0800

    add pallette styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 //components
 import Navbar from './components/Navbar';
 
 //pages
 import home from './pages/home';
 import login from './pages/login';
+import store from './pages/store';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#f06292"
+    },
+    secondary: {
+      main: "#82b1ff"
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route path="/login" component={login} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/store" component={store} />
+              <Route path="/login" component={login} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
---
+++ src/pages/store.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class store extends Component {
+  render() {
+    return (
+      <div>
+        <h1>store Page</h1>
+      </div>
+    );
+  }
+}
+
+export default store;
