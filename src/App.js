29396160b37c7edcad11c3e226e54cb3ca0622bf
jirwincline <jakobinwins@gmail.com>
2019-11-10 18:03:58 -0800

    axios installed to fetch data and posts are now diplaying under store path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 //components
 import Navbar from './components/Navbar';
@@ -19,7 +19,10 @@ const theme = createMuiTheme({
     secondary: {
       main: "#82b1ff"
     }
-  }
+  },
+  typography: {
+    useNextVariants: true,
+  },
 });
 
 function App() {
--- src/pages/store.jsx
+++ src/pages/store.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
 
 export class store extends Component {
+    state = {
+        posts: null
+    }
+    componentDidMount(){
+        axios.get('/posts')
+        .then(res => {
+            console.log(res.data)
+            this.setState({
+                posts: res.data
+            })
+        })
+        .catch(err => console.log(err));
+    }
   render() {
+      let recentPostsMarkup = this.state.posts ? (
+          this.state.posts.map(post => <p>{post.name}</p>)
+      ) : <p>...Loading</p>;
     return (
-      <div>
-        <h1>store Page</h1>
-      </div>
+      <Grid container spacing={10}>
+        <Grid item sm={8} xs={12}>
+            {recentPostsMarkup}
+          <p>Content</p>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile</p>
+        </Grid>
+      </Grid>
     );
   }
 }
